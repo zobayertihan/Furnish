6871d9836f3bc522e38b15501d95a6134b989f35
zobayertihan <zobayertihan@gmail.com>
2023-04-07 02:09:02 +0600

    Contact page buildup done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className=" bg-[#0a192f] text-white">
             <div className="container flex items-center gap-8 p-4 md:p-8 flex-col divide-gray-400">
                 <div className='flex items-center'>
-                    <span className="text-white">© Copyright 2022. BiCycle Keepr. All Rights Reserved.</span>
+                    <span className="text-white">© Copyright 2023. Furnish. All Rights Reserved.</span>
                 </div>
                 <div className="flex justify-end items-center pt-6 lg:pt-0">
                     <div className="flex space-x-4">
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        emailjs.sendForm('service_krm90qx', 'template_zevkhv1', form.current, 'WmP-IBcnkmV4h3qy4')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        form.reset();
+    };
     return (
-        <div className='h-screen'>
-            <h1>Contact Page</h1>
+        <div id='contacts' className='max-w-[1000px] mx-auto my-10 h-screen w-full flex items-center justify-center flex-col bg-gray-300'>
+            <h1 className='text-5xl border-b-4 border-gray-700'>Contact</h1>
+            <p className='my-5 text-center font-semibold text-xl'>Send me a email</p>
+            <form className="container flex flex-col mx-auto space-y-6 ng-untouched ng-pristine ng-valid" ref={form} onSubmit={sendEmail}>
+                <div className="grid grid-cols-1 gap-10 col-span-full ">
+                    <div className="col-span-full w-3/4 mx-auto">
+                        <label for="name" className="text-lg">Name</label>
+                        <input name="user_name" id="name" type="text" placeholder=" Your Name" className="p-2 h-full w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
+                    </div>
+                    <div className="col-span-full w-3/4 mx-auto">
+                        <label for="email" className="text-lg">Email</label>
+                        <input name="user_email" id="email" type="email" placeholder=" Email" className="p-2 h-full w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
+                    </div>
+                    <div className="col-span-full w-3/4 mx-auto">
+                        <label for="message" className="text-lg">Message</label>
+                        <textarea name="message" id="message" placeholder=" Your Message" className="h-full w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 border-gray-700 text-gray-900m p-5" data-gramm="false" wt-ignore-input="true"></textarea>
+                    </div>
+                    <div className='flex items-center justify-center'>
+                        <input className='btn btn-primary rounded-md px-5 py-1 bg-gray-700 text-white' type="submit" value="Send" />
+                    </div>
+                </div>
+
+            </form>
+            <div className="divider mt-10">OR</div>
+            <div>
+                <p className='p-5 text-center'>Contact me With</p>
+                <p className='text-lg font-semibold text-center'> zobayertihan@gmail.com</p>
+            </div>
         </div>
     );
 };
